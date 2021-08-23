451f945a6249f69fcb3b963ca0a1d97764500420
Eric <edhavey@homemail.com>
2021-08-22 21:46:39 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const newHeader = document.querySelector("#victory")
+let main = document.querySelector("#main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Eric is the champion";
+document.body.append(newHeader);
